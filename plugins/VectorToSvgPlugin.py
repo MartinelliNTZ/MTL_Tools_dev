df78fe8508cf545bdb7116477c6c96277734b419
Matheus Augusto Santos Martinelli <44898398+MartinelliNTZ@users.noreply.github.com>
2026-04-04 16:59:50 -0300

    preferences

--- plugins/VectorToSvgPlugin.py
+++ plugins/VectorToSvgPlugin.py
@@ -9,6 +9,7 @@ from qgis.core import QgsMapLayerProxyModel
 
 from .BasePlugin import BasePluginMTL
 from ..core.ui.WidgetFactory import WidgetFactory
+from ..utils.Preferences import load_tool_prefs, save_tool_prefs
 from ..utils.ToolKeys import ToolKey
 
 
@@ -120,6 +121,72 @@ class VectorToSvgPlugin(BasePluginMTL):
                 buttons_layout,
             ]
         )
+
+    def _load_prefs(self):
+        self.logger.debug("Carregando preferencias do VectorToSvg")
+        self.preferences = load_tool_prefs(self.TOOL_KEY)
+
+        layer_id = self.preferences.get("layer_id", "")
+        if layer_id:
+            layer = QgsProject.instance().mapLayer(layer_id)
+            if layer is not None:
+                self.layer_input.set_layer(layer)
+
+        self.fill_color_widget.set_color(
+            QColor(self.preferences.get("fill_color", "#ffffff"))
+        )
+        self.border_color_widget.set_color(
+            QColor(self.preferences.get("border_color", "#000000"))
+        )
+        self.label_color_widget.set_color(
+            QColor(self.preferences.get("label_color", "#000000"))
+        )
+
+        self.options_map["transparent_background"].setChecked(
+            self.preferences.get("transparent_background", False)
+        )
+        self.options_map["show_border"].setChecked(
+            self.preferences.get("show_border", False)
+        )
+        self.options_map["show_label"].setChecked(
+            self.preferences.get("show_label", False)
+        )
+
+        project_folder = QgsProject.instance().homePath()
+        default_folder = os.path.join(project_folder, "svgs")
+        output_folder = self.preferences.get("output_folder", default_folder)
+        self.folder_selector.set_paths([output_folder])
+
+    def _save_prefs(self):
+        self.logger.debug("Salvando preferencias do VectorToSvg")
+
+        current_layer = self.layer_input.current_layer()
+        self.preferences["layer_id"] = current_layer.id() if current_layer else ""
+        self.preferences["fill_color"] = self.fill_color_widget.get_color().name(
+            QColor.HexArgb
+        )
+        self.preferences["border_color"] = self.border_color_widget.get_color().name(
+            QColor.HexArgb
+        )
+        self.preferences["label_color"] = self.label_color_widget.get_color().name(
+            QColor.HexArgb
+        )
+        self.preferences["transparent_background"] = self.options_map[
+            "transparent_background"
+        ].isChecked()
+        self.preferences["show_border"] = self.options_map["show_border"].isChecked()
+        self.preferences["show_label"] = self.options_map["show_label"].isChecked()
+
+        paths = self.folder_selector.get_paths()
+        project_folder = QgsProject.instance().homePath()
+        self.preferences["output_folder"] = (
+            paths[0] if paths else os.path.join(project_folder, "svgs")
+        )
+
+        self.preferences["window_width"] = self.width()
+        self.preferences["window_height"] = self.height()
+
+        save_tool_prefs(self.TOOL_KEY, self.preferences)
 
     def execute_tool(self):
         self.logger.debug(
